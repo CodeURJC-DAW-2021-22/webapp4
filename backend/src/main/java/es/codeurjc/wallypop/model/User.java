31bcf56c6c22f80cac0883be398f819f5b6af3db
jbarcia2019 <80538755+jbarcia2019@users.noreply.github.com>
2022-02-22 15:27:19 +0100

    Create User.java
    Create java file jpa user

---
+++ backend/src/main/java/es/codeurjc/wallypop/model/User.java
@@ -0,0 +1,121 @@
+package es.codeurjc.wallypop.model;
+
+import javax.persistence.*;
+
+import org.springframework.lang.NonNull;
+
+public class User {
+
+	@Id
+ 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
+	@Column(name = "ID_USER")
+ 	private Long ID_USER;
+
+	@NonNull
+	@Column(name = "FULL_NAME")
+ 	private String FULL_NAME;
+	
+	@NonNull
+	@Column(name = "PASSWORD")
+ 	private String PASSWORD;
+ 	
+	@NonNull
+	@Column(name = "EMAIL")
+ 	private String EMAIL;
+ 	
+	@NonNull
+	@Column(name = "TEL")
+ 	private String TEL;
+ 	
+	@NonNull
+	@Column(name = "N_SOLD")
+ 	private int N_SOLD = 0;
+ 	
+	@NonNull
+	@Column(name = "N_SELL")
+ 	private int N_SELL = 0;
+ 	
+	@NonNull
+	@Column(name = "IS_ADMIN")
+ 	private boolean IS_ADMIN = false;
+
+	public User(String fULL_NAME, String pASSWORD, String eMAIL, String tEL, int n_SOLD, int n_SELL,
+			boolean iS_ADMIN) {
+		super();
+		FULL_NAME = fULL_NAME;
+		PASSWORD = pASSWORD;
+		EMAIL = eMAIL;
+		TEL = tEL;
+		N_SOLD = n_SOLD;
+		N_SELL = n_SELL;
+		IS_ADMIN = iS_ADMIN;
+	}
+
+	public User(String fULL_NAME, String pASSWORD, String eMAIL, String tEL, int n_SOLD, int n_SELL) {
+		super();
+		FULL_NAME = fULL_NAME;
+		PASSWORD = pASSWORD;
+		EMAIL = eMAIL;
+		TEL = tEL;
+		N_SOLD = n_SOLD;
+		N_SELL = n_SELL;
+	}
+
+	public Long getID_USER() {
+		return ID_USER;
+	}
+
+	public String getFULL_NAME() {
+		return FULL_NAME;
+	}
+
+	public void setFULL_NAME(String fULL_NAME) {
+		FULL_NAME = fULL_NAME;
+	}
+
+	public String getPASSWORD() {
+		return PASSWORD;
+	}
+
+	public void setPASSWORD(String pASSWORD) {
+		PASSWORD = pASSWORD;
+	}
+
+	public String getEMAIL() {
+		return EMAIL;
+	}
+
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+
+	public String getTEL() {
+		return TEL;
+	}
+
+	public void setTEL(String tEL) {
+		TEL = tEL;
+	}
+
+	public int getN_SOLD() {
+		return N_SOLD;
+	}
+
+	public void setN_SOLD(int n_SOLD) {
+		N_SOLD = n_SOLD;
+	}
+
+	public int getN_SELL() {
+		return N_SELL;
+	}
+
+	public void setN_SELL(int n_SELL) {
+		N_SELL = n_SELL;
+	}
+
+	public boolean isIS_ADMIN() {
+		return IS_ADMIN;
+	} 	
+	
+}
