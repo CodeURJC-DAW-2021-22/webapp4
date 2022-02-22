fdd8adbfb9e72e5799046c3d1c7bff32dfd0c48c
sergio-gomezg <sergio.gagz.99@gmail.com>
2022-02-22 16:11:40 +0100

    Create Category.java and Article_Category.java
    Create entities jpa category and article_category

---
+++ backend/src/main/java/es/codeurjc/wallypop/model/Article_Category.java
@@ -0,0 +1,61 @@
+package es.codeurjc.wallypop.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "Article_Category")
+public class Article_Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
+	@Column(name = "ID_CATEGORY")
+	private long ID_ARTICLE_CATEGORY;
+	
+	@ManyToMany(mappedBy = "ID_ARTICLE")
+	@NonNull
+	@Column(name = "ID_ARTICLE")
+	private Article ARTICLE;
+	
+	@ManyToMany(mappedBy = "ID_CATEGORY")
+	@NonNull
+	@Column(name = "ID_CATEGORY")
+	private Category CATEGORY;
+	
+	
+	public Article_Category(Article aRTICLE, Category cATEGORY) {
+		super();
+		ARTICLE = aRTICLE;
+		CATEGORY = cATEGORY;
+	}
+	
+	public long getID_ARTICLE_CATEGORY() {
+		return ID_ARTICLE_CATEGORY;
+	}
+	public void setID_ARTICLE_CATEGORY(long iD_ARTICLE_CATEGORY) {
+		ID_ARTICLE_CATEGORY = iD_ARTICLE_CATEGORY;
+	}
+	public Article getARTICLE() {
+		return ARTICLE;
+	}
+	public void setARTICLE(Article aRTICLE) {
+		ARTICLE = aRTICLE;
+	}
+	public Category getCATEGORY() {
+		return CATEGORY;
+	}
+	public void setCATEGORY(Category cATEGORY) {
+		CATEGORY = cATEGORY;
+	}
+	
+	
+	
+	
+}
---
+++ backend/src/main/java/es/codeurjc/wallypop/model/Category.java
@@ -0,0 +1,75 @@
+package es.codeurjc.wallypop.model;
+
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
+	@Column(name = "ID_CATEGORY")
+	private long ID_CATEGORY;
+	
+	@NonNull
+	@Column(name = "TITLE")
+	private String TITLE;
+	
+	
+	@Column(name = "DESCRIPTION")
+	private String DESCRIPTION;
+	
+	@Lob
+	@Column(name = "PHOTO")
+	private Blob PHOTO;
+
+	public Category() {}
+	
+	public Category(String TITLE, String DESCRIPTION) {
+		this.TITLE = TITLE;
+		this.DESCRIPTION = DESCRIPTION;
+	}
+
+	public Long getID_CATEGORY() {
+		return ID_CATEGORY;
+	}
+
+	public void setID_CATEGORY(Long iD_CATEGORY) {
+		ID_CATEGORY = iD_CATEGORY;
+	}
+
+	public String getTITLE() {
+		return TITLE;
+	}
+
+	public void setTITLE(String tITLE) {
+		TITLE = tITLE;
+	}
+
+	public String getDESCRIPTION() {
+		return DESCRIPTION;
+	}
+
+	public void setDESCRIPTION(String dESCRIPTION) {
+		DESCRIPTION = dESCRIPTION;
+	}
+
+	public Blob getPHOTO() {
+		return PHOTO;
+	}
+
+	public void setPHOTO(Blob pHOTO) {
+		PHOTO = pHOTO;
+	}
+}
