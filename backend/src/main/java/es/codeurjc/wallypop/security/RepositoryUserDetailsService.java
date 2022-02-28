23290101bb6d7eccc95bf7f82556e0bdcc5f9651
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-02-28 19:22:03 +0100

    Update RepositoryUserDetailsService.java
    Add methods for email search

--- backend/src/main/java/es/codeurjc/wallypop/security/RepositoryUserDetailsService.java
+++ backend/src/main/java/es/codeurjc/wallypop/security/RepositoryUserDetailsService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,8 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.wallypop.model.User;
-import es.codeurjc.wallypop.repositories.UserRepository;
-
+import es.codeurjc.wallypop.repository.UserRepository;
 
 @Service
 public class RepositoryUserDetailsService implements UserDetailsService {
@@ -21,15 +21,33 @@ public class RepositoryUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 
+	public UserDetails loadUserByEmail(String email) throws NotFoundException {
+		User user = userRepository.findByEMAIL(email)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+		List<GrantedAuthority> roles = new ArrayList<>();
+		roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+		if (user.isIS_ADMIN()) {
+			roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+
+		return new org.springframework.security.core.userdetails.User(user.getNAME(), 
+				user.getPASSWORD(), roles);
+	}
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEMAIL(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
-		User user = userRepository.findByName(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+		List<GrantedAuthority> roles = new ArrayList<>();
+		roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+		if (user.isIS_ADMIN()) {
+			roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
 
-
-		return new org.springframework.security.core.userdetails.User(user.getFULL_NAME(), 
-				user.getPASSWORD(), null);
-
+		return new org.springframework.security.core.userdetails.User(user.getNAME(), 
+				user.getPASSWORD(), roles);
 	}
+	
 }
