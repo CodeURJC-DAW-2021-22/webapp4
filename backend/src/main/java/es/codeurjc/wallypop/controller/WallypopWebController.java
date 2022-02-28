5a87be80ceb3e6fc059be8ee7e07dde4e92d80f9
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-02-28 19:31:27 +0100

    Update WallypopWebController.java

--- backend/src/main/java/es/codeurjc/wallypop/controller/WallypopWebController.java
+++ backend/src/main/java/es/codeurjc/wallypop/controller/WallypopWebController.java
@@ -1,9 +1,36 @@
 package es.codeurjc.wallypop.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import es.codeurjc.wallypop.repository.UserRepository;
+
+@Controller
 public class WallypopWebController {
+
+	
+	@ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+
+			model.addAttribute("logged", true);
+			model.addAttribute("userName", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
 
 	@GetMapping("/")
 	public String showIndex(Model model) {
