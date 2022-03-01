a5039cfa53a265f96452c0356529483b9ac1f4a0
jbarcia2019 <80538755+jbarcia2019@users.noreply.github.com>
2022-03-01 21:30:49 +0100

    Sing up

--- backend/src/main/java/es/codeurjc/wallypop/controller/NewAccountWebController.java
+++ backend/src/main/java/es/codeurjc/wallypop/controller/NewAccountWebController.java
@@ -1,13 +1,35 @@
 package es.codeurjc.wallypop.controller;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.codeurjc.wallypop.model.User;
+import es.codeurjc.wallypop.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class NewAccountWebController {
 	
+	@Autowired
+	UserRepository userRepository;
+	
 	@RequestMapping("/newaccount")
-	public String newaccount() {
-		return "newaccount";
+    public String postBody(Model model) {
+		model.addAttribute("user", new User());
+        return "newaccount";
+    }
+	
+	@PostMapping("/register")
+	public User singUp(@ModelAttribute User us) {
+		//userRepository.save(us);
+		System.out.println(us.getFULL_NAME());
+		System.out.println(us.getEncodedPASSWORD());
+		System.out.println(us.getNAME()); // EMAIL
+		System.out.println(us.getFULL_NAME());
+		return null;
 	}
 }
