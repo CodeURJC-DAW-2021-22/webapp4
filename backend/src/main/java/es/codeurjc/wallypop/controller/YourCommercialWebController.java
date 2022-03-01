46f3d974f0b743e3c18d1bd9fa3fb6cb998da1e8
IvanmgmSoftwareEngineering <ivanmgm88@gmail.com>
2022-03-01 11:59:42 +0100

    Successful when saving info. form in the Repository

--- backend/src/main/java/es/codeurjc/wallypop/controller/YourCommercialWebController.java
+++ backend/src/main/java/es/codeurjc/wallypop/controller/YourCommercialWebController.java
@@ -2,11 +2,19 @@ package es.codeurjc.wallypop.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import es.codeurjc.wallypop.model.Article;
+import es.codeurjc.wallypop.repositories.ArticleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 @Controller
 public class YourCommercialWebController {
+	
+	@Autowired
+	private ArticleRepository articleRepository;
 
 	@RequestMapping("/yourcommercial")
 	 public String yourcommercial(Model model, @RequestParam String cITY,@RequestParam String tITLE, @RequestParam String dESCRIPTION, @RequestParam float pRICE ) {
@@ -14,6 +22,7 @@ public class YourCommercialWebController {
 		model.addAttribute("TITLE", tITLE);
 		model.addAttribute("DESCRIPTION", dESCRIPTION);
 		model.addAttribute("PRICE", pRICE);
+		articleRepository.save(new Article("001", cITY, tITLE, dESCRIPTION, (float) pRICE, "0001"));
 		return "yourcommercial";
 	}
 	
