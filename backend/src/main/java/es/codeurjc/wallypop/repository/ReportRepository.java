643074c3067960cfd3350eea119a4295bcbb6630
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-03-04 16:36:22 +0100

    Update ReportRepository.java

--- backend/src/main/java/es/codeurjc/wallypop/repository/ReportRepository.java
+++ backend/src/main/java/es/codeurjc/wallypop/repository/ReportRepository.java
@@ -1,12 +1,12 @@
 package es.codeurjc.wallypop.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import es.codeurjc.wallypop.model.Report;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-
+	Optional<List<Report>> findAllByArticleId(long article_id);
 }
