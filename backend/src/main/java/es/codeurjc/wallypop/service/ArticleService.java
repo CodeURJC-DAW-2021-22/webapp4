25baab7e830606f91e66338c8ef0b45c6d53fb2c
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-03-13 16:57:00 +0100

    Update ArticleService.java

--- backend/src/main/java/es/codeurjc/wallypop/service/ArticleService.java
+++ backend/src/main/java/es/codeurjc/wallypop/service/ArticleService.java
@@ -1,6 +1,5 @@
 package es.codeurjc.wallypop.service;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.wallypop.model.Article;
-import es.codeurjc.wallypop.model.Category;
 import es.codeurjc.wallypop.model.User;
 import es.codeurjc.wallypop.repository.ArticleRepository;
 import es.codeurjc.wallypop.repository.UserRepository;
@@ -44,35 +42,32 @@ public class ArticleService {
 	}
 
 	public List<Article> findArticlesByCategory(long id) {
-		Optional<Category> c = categoryService.findById(id);
-		List<Article> lResult = new LinkedList<>();
-		if (c.isPresent()) {
-			for (Article a : findAll()) {
-				if (a.getCATEGORYS().contains(c.get())) {
-					lResult.add(a);
-				}
-			}
-		}
-		return lResult;
+		return articleRepository.findByCATEGORYS(categoryService.findById(id).get());
 	}
 
 	public Optional<Article> findById(long id) {
 		return articleRepository.findById(id);
 	}
 
-	public void save(Article article) {
-		articleRepository.save(article);
+	public List<Article> findBySOLDFalse() {
+		return articleRepository.findBySOLDFalse();
 	}
 
-	public void delete(long id) {
-		articleRepository.deleteById(id);
+	public List<Article> findBySOLDTrue() {
+		return articleRepository.findBySOLDTrue();
 	}
 
-	public void deletePost(long id, Long id_user, boolean admin) {
-		Article a = findById(id).get();
-		if (a.getUserID() == id_user || admin) {
-			delete(id);
-		}
+	public List<Article> findByTITLEContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalseOrDESCRIPTIONContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalse(
+			String query, String city) {
+		return articleRepository
+				.findByTITLEContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalseOrDESCRIPTIONContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalse(
+						query, city, query, city);
+	}
+
+	public List<Article> findByTitleContainingOrDescriptionContainingOrCITYContaining(String query, String city) {
+		return articleRepository
+				.findByTITLEContainingIgnoreCaseOrDESCRIPTIONContainingIgnoreCaseOrCITYContainingIgnoreCaseAndSOLDFalse(
+						query, query, city);
 	}
 
 	public void reserve(long id, boolean bool, long id_user, boolean admin) {
@@ -97,30 +92,5 @@ public class ArticleService {
 			a.setRESERVED(false);
 			save(a);
 		}
-	}
-
-	public List<Article> findArticlesByCategory(long id) {
-		return articleRepository.findByCATEGORYS(categoryService.findById(id).get());
-	}
-
-	public List<Article> findBySOLDFalse() {
-		return articleRepository.findBySOLDFalse();
-	}
-
-	public List<Article> findBySOLDTrue() {
-		return articleRepository.findBySOLDTrue();
-	}
-
-	public List<Article> findByTitleContainingOrDescriptionContainingOrCITYContaining(String query, String city) {
-		return articleRepository
-				.findByTITLEContainingIgnoreCaseOrDESCRIPTIONContainingIgnoreCaseOrCITYContainingIgnoreCaseAndSOLDFalse(
-						query, query, city);
-	}
-
-	public List<Article> findByTITLEContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalseOrDESCRIPTIONContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalse(
-			String query, String city) {
-		return articleRepository
-				.findByTITLEContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalseOrDESCRIPTIONContainingIgnoreCaseAndCITYContainingIgnoreCaseAndSOLDFalse(
-						query, city, query, city);
 	}
 }
