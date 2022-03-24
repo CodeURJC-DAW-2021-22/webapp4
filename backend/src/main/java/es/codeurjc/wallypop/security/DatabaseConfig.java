940af16d62feb782a81efd58886660fdcd36cbdc
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-03-24 22:49:55 +0100

    Heroku configuration

---
+++ backend/src/main/java/es/codeurjc/wallypop/security/DatabaseConfig.java
@@ -0,0 +1,39 @@
+package es.codeurjc.wallypop.security;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Configuration
+public class DatabaseConfig {
+    private static Logger log = (Logger) LoggerFactory.getLogger(DatabaseConfig.class);
+    @Value("${DATABASE_URL:null}")
+    private String databaseUrl;
+    @Bean
+    @ConditionalOnProperty("DATABASE_URL")
+    public DataSource dataSource() throws URISyntaxException {
+        log.log(Level.INFO, "Using database configured in DATABASE_URL=" + databaseUrl);
+        HikariConfig config = new HikariConfig();
+        URI uri = new URI(databaseUrl);
+        String url = "jdbc:" + new URI("postgresql", null, uri.getHost(), uri.getPort(), uri.getPath(),
+                uri.getQuery(), uri.getFragment()).toString();
+        String[] userInfoParts = uri.getUserInfo().split(":");
+        String username = userInfoParts[0];
+        String password = userInfoParts[1];
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setAutoCommit(false);
+        return new HikariDataSource(config);
+    }
+}
