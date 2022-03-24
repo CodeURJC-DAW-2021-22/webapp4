ed7585802a7d125d585268e30355f805ad31d039
sergio-gomezg <sergio.gagz.99@gmail.com>
2022-03-24 14:03:34 +0100

    Image methods working in Category API

--- backend/src/main/java/es/codeurjc/wallypop/controller/CategoryRestController.java
+++ backend/src/main/java/es/codeurjc/wallypop/controller/CategoryRestController.java
@@ -1,11 +1,19 @@
 package es.codeurjc.wallypop.controller;
 
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+
+import java.io.IOException;
+import java.net.URI;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,8 +23,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import es.codeurjc.wallypop.model.Category;
 import es.codeurjc.wallypop.service.CategoryService;
@@ -73,4 +83,48 @@ public class CategoryRestController {
         }
 
     }
+    
+    
+	@PostMapping("/{id}/image")
+	public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+			throws IOException {
+
+		Category category = categoryService.findById(id).orElseThrow();
+
+		URI location = fromCurrentRequest().build().toUri();
+
+		category.setPHOTO(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+		categoryService.save(category);
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping("/{id}/image")
+	public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
+
+		Category category = categoryService.findById(id).orElseThrow();
+
+		if (category.getPHOTO() != null) {
+
+			Resource file = new InputStreamResource(category.getPHOTO().getBinaryStream());
+
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+					.contentLength(category.getPHOTO().length()).body(file);
+
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}/image")
+	public ResponseEntity<Object> deleteImage(@PathVariable long id) throws IOException {
+
+		Category category = categoryService.findById(id).orElseThrow();
+
+		category.setPHOTO(null);
+
+		categoryService.save(category);
+
+		return ResponseEntity.noContent().build();
+	}
 }
