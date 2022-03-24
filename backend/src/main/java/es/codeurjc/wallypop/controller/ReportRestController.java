d43b39a463ac7630f6749d3b86ba384e6c8b6c46
Juan Diez <80917809+Juandiezdc@users.noreply.github.com>
2022-03-24 14:49:23 +0100

    API Report Update
    Added images in the reports api

--- backend/src/main/java/es/codeurjc/wallypop/controller/ReportRestController.java
+++ backend/src/main/java/es/codeurjc/wallypop/controller/ReportRestController.java
@@ -1,11 +1,17 @@
 package es.codeurjc.wallypop.controller;
 
+import java.io.IOException;
+import java.net.URI;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,10 +21,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
+
+import es.codeurjc.wallypop.model.Article;
 import es.codeurjc.wallypop.model.Report;
+import es.codeurjc.wallypop.service.ArticleService;
 import es.codeurjc.wallypop.service.ReportService;
 
 @RestController
@@ -27,6 +39,8 @@ public class ReportRestController {
 	
 	@Autowired
 	private ReportService reportService;
+	@Autowired
+	private ArticleService articleService;
 	
 	 @GetMapping("/")
 	    List<Report> all() {
@@ -35,7 +49,6 @@ public class ReportRestController {
 	 
 	    @GetMapping("/{id}")
 	    public ResponseEntity<Report> getReport(@PathVariable long id) {
-
 	        Optional<Report> or = reportService.findById(id);
 	        if (or.isPresent()) {
 	            Report report = or.get();
@@ -44,7 +57,55 @@ public class ReportRestController {
 	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	        }
 	    }
+	    
+		@GetMapping("/{id}/reporte/proof")
+		public ResponseEntity<Object> downloadReporteProof(@PathVariable long id) throws SQLException {
 
+			Report report = reportService.findById(id).orElseThrow();
+
+			if (report.getPROOF() != null) {
+
+				Resource file = new InputStreamResource(report.getPROOF().getBinaryStream());
+
+				return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+						.contentLength(report.getPROOF().length()).body(file);
+
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		}
+		
+	    
+	    @GetMapping("/{id}/reportedArticle")
+	    public ResponseEntity<Article> getArticleReported(@PathVariable long id) {
+	        Optional<Article> oa = articleService.findById(reportService.findById(id).get().getARTICLE().getID_ARTICLE());
+	        if (oa.isPresent()) {
+	            Article articleReported = oa.get();
+	            return new ResponseEntity<>(articleReported, HttpStatus.OK);
+	        } else {
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	    }
+
+		@GetMapping("/{id}/reportedArticle/image")
+		public ResponseEntity<Object> downloadReportedArticleImage(@PathVariable long id) throws SQLException {
+
+			Article article = articleService.findById(reportService.findById(id).get().getARTICLE().getID_ARTICLE()).orElseThrow();
+
+			if (article.getPHOTO() != null) {
+
+				Resource file = new InputStreamResource(article.getPHOTO().getBinaryStream());
+
+				return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+						.contentLength(article.getPHOTO().length()).body(file);
+
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		}
+		
+
+	    
 	    @PostMapping("")
 	    @ResponseStatus(HttpStatus.CREATED)
 	    public Report createReport(@RequestBody Report report) {
@@ -52,19 +113,37 @@ public class ReportRestController {
 	        return report;
 	    }
 	    
-	    @PutMapping("/{id}")
-	    public ResponseEntity<Report> updateReport(@PathVariable long id, @RequestBody Report updatedReport) throws SQLException {
-	        if (reportService.exist(id)) {
-	            return reportService.updateReport(id, updatedReport);
-	        } else {
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-	    }
+		@PostMapping("/{id}/image")
+		public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+				throws IOException {
+			Report report = reportService.findById(id).orElseThrow();
+
+			URI location = fromCurrentRequest().build().toUri();
+			report.setPROOF(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+			reportService.save(report);
+
+			return ResponseEntity.created(location).build();
+		}
 	    
-	    @DeleteMapping("/{id}")
+	    
+	    @DeleteMapping("/{id}/rejectReport")
 	    public ResponseEntity<Report> deleteReport(@PathVariable long id) {
 
 	        try {
+	            reportService.deleteById(id);
+	            return new ResponseEntity<>(null, HttpStatus.OK);
+
+	        } catch (EmptyResultDataAccessException e) {
+	            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	        }
+
+	    }
+	    
+	    @DeleteMapping("/{id}/aceptReport")
+	    public ResponseEntity<Article> deleteArticleReported(@PathVariable long id) {
+
+	        try {
+	        	articleService.delete(reportService.findById(id).get().getARTICLE().getID_ARTICLE());
 	            reportService.deleteById(id);
 	            return new ResponseEntity<>(null, HttpStatus.OK);
 
