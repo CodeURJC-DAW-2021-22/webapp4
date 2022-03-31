578bde04a9f660b4b5fafd6b78feb71f3e4d3288
jbarcia2019 <80538755+jbarcia2019@users.noreply.github.com>
2022-03-31 22:30:30 +0200

    Update create_heroku_app.sh

--- backend/create_heroku_app.sh
+++ backend/create_heroku_app.sh
@@ -6,3 +6,4 @@ heroku addons:create heroku-postgresql --app $1
 
 heroku config:set SERVER_SSL_ENABLED=false --app $1
 heroku config:set SPRING_JPA_HIBERNATE_DDL-AUTO=update --app $1
+heroku config:set JAVA_TOOL_OPTIONS="-XX:ReservedCodeCacheSize=64M -XX:MaxMetaspaceSize=100000K" --app $1
