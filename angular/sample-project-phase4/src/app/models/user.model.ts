89b9aaa7d9dc4bdd5d9861abc810ef799586d2f4
jbarcia2019 <j.barcia.2019@alumnos.urjc.es>
2022-04-20 21:58:36 +0200

    Add user model

--- angular/sample-project-phase4/src/app/models/user.model.ts
+++ angular/sample-project-phase4/src/app/models/user.model.ts
@@ -1,5 +1,12 @@
 export interface User {
     id?: number;
-    name: string;
-    roles: string[];
+    full_name: string;
+    password: string;
+    email: string;
+    tel: string;
+    n_sold: number;
+    n_sell: number;
+    is_admin: boolean;
+    //articles: Article[];
+    //favorites: Favorites[];
 }
