5e785ce1896b3d778868bcc0b6896ab72277ca68
sergio-gomezg <sergio.gagz.99@gmail.com>
2022-05-02 18:47:01 +0200

    show categories in category page

--- angular/wallypop/src/app/components/category/category.component.ts
+++ angular/wallypop/src/app/components/category/category.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Category } from 'src/app/models/category.model';
 import {CategoryService} from '../../services/category.service';
 import {LoginService} from '../../services/login.service';
@@ -7,10 +7,20 @@ import {LoginService} from '../../services/login.service';
     selector: 'category',
     templateUrl: './category.component.html'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+
+    categories: Category[];
+
     constructor(public categoryService: CategoryService, public loginService: LoginService) {
     }
 
+    ngOnInit(): void {
+        this.categoryService.getCategories().subscribe(
+            category => this.categories = category,
+            error => console.log(error)
+          );
+    }
+    
     addCategory(event: any, title: string, description: string, icon: string): void {
         event.preventDefault();
         this.categoryService.addCategory(title,description,icon);
