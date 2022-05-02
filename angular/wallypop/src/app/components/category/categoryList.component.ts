a0e3b89da8450466d52728080360ceac759a5b7f
sergio-gomezg <sergio.gagz.99@gmail.com>
2022-05-02 20:33:19 +0200

    Remove category working

--- angular/wallypop/src/app/components/category/categoryList.component.ts
+++ angular/wallypop/src/app/components/category/categoryList.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import { Category } from 'src/app/models/category.model';
 import {CategoryService} from '../../services/category.service';
 import {LoginService} from '../../services/login.service';
@@ -11,7 +12,7 @@ export class CategoryListComponent implements OnInit{
 
     categories: Category[];
 
-    constructor(public categoryService: CategoryService, public loginService: LoginService) {
+    constructor(public categoryService: CategoryService, public loginService: LoginService, private router: Router) {
     }
     ngOnInit(): void {
         this.categoryService.getCategories().subscribe(
@@ -23,6 +24,16 @@ export class CategoryListComponent implements OnInit{
     addCategory(event: any, title: string, description: string, icon: string): void {
         event.preventDefault();
         this.categoryService.addCategory(title,description,icon);
+    }
+
+    removeCategory(removeCategorySelected: Category): void {
+        const okResponse = window.confirm('Do you want to remove this category?');
+        if (okResponse) {
+            this.categoryService.deleteCategory(removeCategorySelected).subscribe(
+                _ => this.router.navigate(['/category']),
+                error => console.error(error)
+            );
+        }
     }
 
 }
