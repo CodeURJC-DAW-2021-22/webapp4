ac1017878740b0393177a2c11d14a0aa3361aa7f
Juandiezdc <80917809+Juandiezdc@users.noreply.github.com>
2022-05-03 19:02:33 +0200

    List of reports for the admin funcionality completed

--- angular/wallypop/src/app/components/report/reports.component.ts
+++ angular/wallypop/src/app/components/report/reports.component.ts
@@ -2,6 +2,7 @@ import {ReportService} from '../../services/report.service';
 import {Component, OnInit} from '@angular/core';
 import { Report } from 'src/app/models/report.model';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'reports',
@@ -9,7 +10,7 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class ReportsComponent implements OnInit {
   reports: Report[]
-  constructor(public reportService: ReportService, public loginService: LoginService) {
+  constructor(public reportService: ReportService, public loginService: LoginService,private router: Router) {
     
    }
    ngOnInit() {
@@ -18,4 +19,10 @@ export class ReportsComponent implements OnInit {
       error => console.log(error)
     );
   }
+
+  showReport(event: any, id: number) {
+    event.preventDefault();
+    this.router.navigate(['/api/admin/reports/'+id.toString]);
+  }
+
   }
